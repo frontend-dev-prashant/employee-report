bde7b617e7299ac83561905b9c30b816078b34c1
Prashant <prashantnadar18@gmail.com>
2025-11-12 00:23:30 +0530

    Updated something

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='text-red-600'>
+        test
+      </div>
+    </>
+  )
+}
+
+export default App;
